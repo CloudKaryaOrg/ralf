d0ea675592694df908019770ac1879092689da1d
peeya-i <pi3860@gmail.com>
2025-08-19 10:39:10 -0700

    Added . to local file import

--- ralf/ralf.py
+++ ralf/ralf.py
@@ -8,7 +8,7 @@ from nltk.corpus import wordnet # Ensure you have the OpenAI Python client insta
 import json
 import re
 import random
-from ralf_train import RalfTraining, get_system_info, RalfSavingCallback
+from .ralf_train import RalfTraining, get_system_info, RalfSavingCallback
 
 warnings.filterwarnings("ignore")  # Ignore warnings for cleaner output
 
