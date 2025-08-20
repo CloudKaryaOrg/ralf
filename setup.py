42bf6e43c241ac518f14bb5f4f32a77a0243c5e2
peeya-i <pi3860@gmail.com>
2025-08-19 17:07:33 -0700

    Update setup and __init__.py to synchronize the version number.

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import setup, find_packages
 # from .ralf.__init__ import __version__
 
+import os
+
+# Read the version from my_package/__init__.py
+def get_version():
+    with open(os.path.join(os.path.dirname(__file__), 'my_package', '__init__.py')) as f:
+        for line in f:
+            if line.startswith('__version__'):
+                return line.split('=')[1].strip().strip('"\'')
+    raise RuntimeError("Unable to find __version__ string.")
+
 setup(
     name="ralf",
-    version="0.1.11",
+    version=get_version(),
     packages=find_packages(),
     install_requires=[
         "pandas",
